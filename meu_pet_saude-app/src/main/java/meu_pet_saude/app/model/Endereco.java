e095608047f8427207b4f475849527126c088e47
Fabiana <fabiana.lua@gmail.com>
2024-03-28 12:06:22 -0300

    PET-001 Atualização da classe Endereço para acessar os dados da API externa ViaCep

--- meu_pet_saude-app/src/main/java/meu_pet_saude/app/model/Endereco.java
+++ meu_pet_saude-app/src/main/java/meu_pet_saude/app/model/Endereco.java
@@ -1,8 +1,11 @@
 package meu_pet_saude.app.model;
 
+import org.springframework.web.client.RestTemplate;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,10 +18,32 @@ import lombok.NoArgsConstructor;
 public class Endereco {
     
     private String cep;
-    private String logradouro;
+    private String endereco;
+    private String numero;
     private String complemento;
     private String bairro;
-    private String localidade;
-    private String uf;
+    private String cidade;
+    private String estado;
+    private String pais;
+
+    public void buscarEnderecoPorCep(String cep) {
+        try {
+            String url = "https://viacep.com.br/ws/" + cep + "/json/";
+        RestTemplate restTemplate =new RestTemplate();
+        ViaCepEndereco viaCepEndereco = restTemplate.getForObject(url, ViaCepEndereco.class);
+
+        if (viaCepEndereco != null) {
+
+            this.endereco = viaCepEndereco.getLogradouro();
+            this.bairro = viaCepEndereco.getBairro();
+            this.cidade = viaCepEndereco.getLocalidade();
+            this.estado = viaCepEndereco.getUf();
+        }
+            
+        } catch (Exception e) {
+            throw new EntityNotFoundException("Cep não encontrado." + e.getMessage());
+        }
+        
+    }
 
 }
