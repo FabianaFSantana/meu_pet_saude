cdb10fe4c4a31dc6464d7afc30ff591f3b8c2ec2
Fabiana <fabiana.lua@gmail.com>
2024-03-28 15:57:54 -0300

    PET-003 Ajuste do método para buscar endereço na classe ViaCepEnderecoService, criação da classe TutorController e dos endpoints POST para cadastrar e cadastrar endereço acessando API ViaCep

---
+++ meu_pet_saude-app/src/main/java/meu_pet_saude/app/controller/TutorController.java
@@ -0,0 +1,41 @@
+package meu_pet_saude.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import meu_pet_saude.app.model.Endereco;
+import meu_pet_saude.app.model.Tutor;
+import meu_pet_saude.app.repository.TutorRepository;
+import meu_pet_saude.app.service.ViaCepEnderecoService;
+
+@RestController
+@RequestMapping("/tutor")
+public class TutorController {
+
+    @Autowired
+    private TutorRepository tutorRepository;
+
+    @Autowired
+    private ViaCepEnderecoService viaCepEnderecoService;
+
+    @PostMapping
+    public ResponseEntity<Tutor> cadastrarTutor(@RequestBody Tutor tutor){
+         return ResponseEntity.status(HttpStatus.CREATED)
+        .body(tutorRepository.save(tutor));
+    }
+
+    @PostMapping("/cadastrarEndereco/{id}")
+    public ResponseEntity<String> atualizarEnderecoDoTutor(@PathVariable("id") Long id, @RequestBody Endereco endereco) {
+        viaCepEnderecoService.buscarEnderecoPorCep(id, endereco.getCep());
+        return ResponseEntity.status(HttpStatus.OK)
+        .body("Endereço cadastrado!");
+       
+    }
+}
--- meu_pet_saude-app/src/main/java/meu_pet_saude/app/service/ViaCepEnderecoService.java
+++ meu_pet_saude-app/src/main/java/meu_pet_saude/app/service/ViaCepEnderecoService.java
@@ -1,36 +1,60 @@
 package meu_pet_saude.app.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import jakarta.persistence.EntityNotFoundException;
 import meu_pet_saude.app.model.Endereco;
+import meu_pet_saude.app.model.Tutor;
 import meu_pet_saude.app.model.ViaCepEndereco;
+import meu_pet_saude.app.repository.TutorRepository;
 
 @Service
 public class ViaCepEnderecoService {
 
-    public Endereco buscarEnderecoPorCep(String cep) {
-        try {
-            String url = "https://viacep.com.br/ws/" + cep + "/json/";
-            RestTemplate restTemplate = new RestTemplate();
-            ViaCepEndereco viaCepEndereco = restTemplate.getForObject(url, ViaCepEndereco.class);
+    @Autowired
+    private TutorRepository tutorRepository;
 
-            if (viaCepEndereco != null) {
-                Endereco endereco = new Endereco();
+    public void buscarEnderecoPorCep(Long id, String cep) {
 
-                endereco.setEndereco(viaCepEndereco.getLogradouro());
-                endereco.setBairro(viaCepEndereco.getBairro());
-                endereco.setCidade(viaCepEndereco.getLocalidade());
-                endereco.setEstado(viaCepEndereco.getUf());
-                return endereco;
+        Optional<Tutor> tutorOptional = tutorRepository.findById(id);
+
+        if (tutorOptional.isPresent()) {
+            Tutor tutorEncont = tutorOptional.get();
+
+            try {
+                String url = "https://viacep.com.br/ws/" + cep + "/json/";
+                RestTemplate restTemplate = new RestTemplate();
+                ViaCepEndereco viaCepEndereco = restTemplate.getForObject(url, ViaCepEndereco.class);
+    
+                if (viaCepEndereco != null) {
+                    Endereco endereco = tutorEncont.getEndereco();
+    
+                    endereco.setCep(viaCepEndereco.getCep());
+                    endereco.setEndereco(viaCepEndereco.getLogradouro());
+                    endereco.setBairro(viaCepEndereco.getBairro());
+                    endereco.setCidade(viaCepEndereco.getLocalidade());
+                    endereco.setEstado(viaCepEndereco.getUf());
+
+                    tutorRepository.save(tutorEncont);
+                    
+                }
+    
+            } catch (Exception e) {
+                throw new RuntimeException("Erro ao procurar o cep" + e.getMessage());
             }
-
-        } catch (Exception e) {
-            throw new EntityNotFoundException("Cep não encontrado." + e.getMessage());
+            
+           
+    
+        } else {
+            throw new EntityNotFoundException("Tutor não encontrdo com este id:" + id);
         }
-        return null;
-
-    }
+    
+            
+        }
+        
 
 }
