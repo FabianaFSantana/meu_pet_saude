6de599c4944f76445f2ca7f0c27139a33013351c
Fabiana <fabiana.lua@gmail.com>
2024-03-28 11:07:58 -0300

    Início da API

---
+++ meu_pet_saude-app/src/main/java/meu_pet_saude/app/MeuPetSaudeAppApplication.java
@@ -0,0 +1,13 @@
+package meu_pet_saude.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPetSaudeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPetSaudeAppApplication.class, args);
+	}
+
+}
---
+++ meu_pet_saude-app/src/test/java/meu_pet_saude/app/MeuPetSaudeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package meu_pet_saude.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPetSaudeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
