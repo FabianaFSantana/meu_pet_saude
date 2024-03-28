d879769be02f8a1922a8c476c01a492013a6d24e
Fabiana <fabiana.lua@gmail.com>
2024-03-28 13:01:01 -0300

    PET-001 Ajuste na classe model Endereço e criação da classe ViaCepEnderecoService para a construção do método para acessar a API externa ViaCep

--- meu_pet_saude-app/src/main/java/meu_pet_saude/app/model/Endereco.java
+++ meu_pet_saude-app/src/main/java/meu_pet_saude/app/model/Endereco.java
@@ -1,22 +1,19 @@
 package meu_pet_saude.app.model;
 
-import org.springframework.web.client.RestTemplate;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor@AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @Embeddable
 
-@JsonIgnoreProperties(ignoreUnknown = true) 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Endereco {
-    
+
     private String cep;
     private String endereco;
     private String numero;
@@ -25,25 +22,5 @@ public class Endereco {
     private String cidade;
     private String estado;
     private String pais;
-
-    public void buscarEnderecoPorCep(String cep) {
-        try {
-            String url = "https://viacep.com.br/ws/" + cep + "/json/";
-        RestTemplate restTemplate =new RestTemplate();
-        ViaCepEndereco viaCepEndereco = restTemplate.getForObject(url, ViaCepEndereco.class);
-
-        if (viaCepEndereco != null) {
-
-            this.endereco = viaCepEndereco.getLogradouro();
-            this.bairro = viaCepEndereco.getBairro();
-            this.cidade = viaCepEndereco.getLocalidade();
-            this.estado = viaCepEndereco.getUf();
-        }
-            
-        } catch (Exception e) {
-            throw new EntityNotFoundException("Cep não encontrado." + e.getMessage());
-        }
-        
-    }
 
 }
