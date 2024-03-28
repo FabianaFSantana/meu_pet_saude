6885377cf3cad1751cb47355fdff37f12a6c42ef
Fabiana <fabiana.lua@gmail.com>
2024-03-28 20:36:02 -0300

    PET-003 Criação dos endpoints PUT para atualizar e Delete para excluir tutor

--- meu_pet_saude-app/src/main/java/meu_pet_saude/app/controller/TutorController.java
+++ meu_pet_saude-app/src/main/java/meu_pet_saude/app/controller/TutorController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,5 +54,38 @@ public class TutorController {
     public ResponseEntity<Optional<Tutor>> buscarTutorPeloId(@PathVariable("id") Long id) {
         return ResponseEntity.status(HttpStatus.OK)
         .body(tutorRepository.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Tutor> atualizarTutorPeloId(@PathVariable("id") Long id,
+    @RequestBody Tutor tutor) {
+        Optional<Tutor> tutorOptional = tutorRepository.findById(id);
+
+        if (tutorOptional.isPresent()) {
+            Tutor tutorEncont = tutorOptional.get();
+
+            tutorEncont.setNome(tutor.getNome());
+            tutorEncont.setDataDeNascimento(tutor.getDataDeNascimento());
+            tutorEncont.setTelefone(tutor.getTelefone());
+            tutorEncont.getEndereco().setCep(tutor.getEndereco().getCep());
+            tutorEncont.getEndereco().setEndereco(tutor.getEndereco().getEndereco());
+            tutorEncont.getEndereco().setNumero(tutor.getEndereco().getNumero());
+            tutorEncont.getEndereco().setComplemento(tutor.getEndereco().getComplemento());
+            tutorEncont.getEndereco().setBairro(tutor.getEndereco().getBairro());
+            tutorEncont.getEndereco().setCidade(tutor.getEndereco().getCidade());
+            tutorEncont.getEndereco().setEstado(tutor.getEndereco().getEstado());
+            tutorEncont.getEndereco().setPais(tutor.getEndereco().getPais());
+
+            return ResponseEntity.status(HttpStatus.OK)
+            .body(tutorRepository.save(tutorEncont));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> excluirTutor(@PathVariable("id") Long id) {
+        tutorRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.OK)
+        .body("Tutor exluído com sucesso!");
     }
 }
