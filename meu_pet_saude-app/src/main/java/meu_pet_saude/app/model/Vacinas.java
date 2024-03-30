6ad894415beef94d83abef9133aa306cea4cc50c
Fabiana <fabiana.lua@gmail.com>
2024-03-30 11:12:50 -0300

    PET-005 Criação da classe model vacinas

---
+++ meu_pet_saude-app/src/main/java/meu_pet_saude/app/model/Vacinas.java
@@ -0,0 +1,49 @@
+package meu_pet_saude.app.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "vacinas")
+
+public class Vacinas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idVacina;
+
+    @Column(nullable = false, unique = true)
+    private String nomeVacina;
+
+    @Column(nullable = false)
+    private String descricaoDoencas;
+
+    @Column(nullable = false)
+    private int idadeMinima;
+
+    @Column(nullable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataDaUltimaDose;
+
+    @Column(nullable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataDaProximaDose;
+
+    @Column(nullable = false)
+    private String nomeDaClinica;
+
+    @Column(nullable = false)
+    private String nomeVeterinario;
+}
