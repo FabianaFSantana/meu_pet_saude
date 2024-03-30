93cd4fc3b2487f0040055bc51b0bdaca47bc4f52
Fabiana <fabiana.lua@gmail.com>
2024-03-30 10:28:08 -0300

    PET-004 Criação dos endpoints PUT e Delete para atualizar e deletar cadastro do animal na classe AnimalController

--- meu_pet_saude-app/src/main/java/meu_pet_saude/app/controller/AnimalController.java
+++ meu_pet_saude-app/src/main/java/meu_pet_saude/app/controller/AnimalController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +41,37 @@ public class AnimalController {
     public ResponseEntity<Optional<Animal>> buscarAnimalPeloId(@PathVariable("idAnimal") Long idAnimal) {
         return ResponseEntity.status(HttpStatus.OK)
         .body(animalRepository.findById(idAnimal));
+    }
+
+    @PutMapping("/{idAnimal}")
+    public ResponseEntity<Animal> atualizarDadosDoAnimal(@PathVariable("idAnimal") Long idAnimal, 
+    @RequestBody Animal animal){
+        Optional<Animal> animOptional = animalRepository.findById(idAnimal);
+
+        if (animOptional.isPresent()) {
+            Animal animalEncont = animOptional.get();
+
+            animalEncont.setNome(animal.getNome());
+            animalEncont.setEspecie(animal.getEspecie());
+            animalEncont.setRaca(animal.getRaca());
+            animalEncont.setDataDeNascimento(animal.getDataDeNascimento());
+            animalEncont.setPeso(animal.getPeso());
+            animalEncont.setCorDoPelo(animal.getCorDoPelo());
+
+            return ResponseEntity.status(HttpStatus.OK)
+            .body(animalRepository.save(animalEncont));
+            
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
+    @DeleteMapping("/{idAnimal}")
+    public ResponseEntity<String> excluirCadastroAnimal(@PathVariable("idAnimal") Long idAnimal) {
+        animalRepository.deleteById(idAnimal);
+        return ResponseEntity.status(HttpStatus.OK)
+        .body("Cadastro do animal excluído com sucesso!");
     }
 
 
