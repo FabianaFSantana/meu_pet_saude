92a9617c8d23d5b5ca367cd9ce168e99d4e34870
Fabiana <fabiana.lua@gmail.com>
2024-04-22 10:27:26 -0300

    PET-016 Criação do método para enviar email de dose de refoço do carrapaticida para o tutor

--- src/main/java/meu_pet_saude/app/MeuPetSaudeAppApplication.java
+++ src/main/java/meu_pet_saude/app/MeuPetSaudeAppApplication.java
@@ -1,7 +1,7 @@
 package meu_pet_saude.app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
 
 @SpringBootApplication
 public class MeuPetSaudeAppApplication {
